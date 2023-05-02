1f292629ec4336b6cb4612a9d6701a569580461d
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2023-05-02 11:36:01 -0300

    Haciendo logs privado para el exterior pero publico para el crate

--- src/bin/test-bed/main.rs
+++ src/bin/test-bed/main.rs
@@ -1,26 +1,4 @@
-use std::path::Path;
-use std::thread;
-use std::time::Duration;
-use test_engine::logs::{error_log, logger};
 
-fn main() -> Result<(), error_log::ErrorLog> {
-    let archivo = Path::new("algo.txt");
-    let (sender, receiver) = logger::inicializar_logger(archivo)?;
-
-    let handle = thread::spawn(move || receiver.recibir_logs());
-
-    for i in 0..10 {
-        let sender_copia = sender.clone();
-        thread::spawn(move || {
-            sender_copia
-                .log_debug(format!("Mensaje de prueba: {}", i))
-                .unwrap();
-            thread::sleep(Duration::from_millis(500));
-        });
-    }
-
-    std::mem::drop(sender);
-    let _ = handle.join().unwrap();
-
-    Ok(())
+fn main() {
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
 /// Agregar un sistema para poder loggear mensajes
-pub mod logs;
+pub(crate) mod logs;
