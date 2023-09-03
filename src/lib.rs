f2c494457ca4ebfc8893966fb002d581a61ba71c
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2023-09-03 18:14:22 -0300

    Empezando con los archivos de windowing

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 /// Agregar un sistema para poder loggear mensajes
 pub mod logs;
+
+/// Sistema en el cual vamos a poder integrar una interfaz con la creacion y utilizacion de
+/// ventanas
+pub mod windowing;
